1d29b387e68c567049881e8019fa42dae7c228a3
Mateus Ballao <mateusrduarte@gmail.com>
2026-09-22 02:46:13 +0000

    update status endpoint to return database version and timestamp

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows[0]);
-  response.status(200).json({ chave: "Testando, mas dessa vez em JSON" });
+  const updateAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+  console.log(dbVersionValue);
+  // const dsMaxConnections = await database.query("SHOW max_connections");
+  // const dbUsedConnections = await database.query(
+  //   "SELECT COUNT(*) FROM pg_stat_activity",
+  // );
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
